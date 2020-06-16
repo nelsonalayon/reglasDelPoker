a3884f5b80c4c49d386e376479f8512bae68b259
Nelson Alayon <neanmi@hotmail.com>
2020-06-16 14:14:20 -0500

    Añadido menú lateral para ver en pantallas pequeñas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+
+$('#offCanvas li a').click(() => {
+  $('#offCanvas').foundation('close')
+})
+
+var elem = new Foundation.SmoothScroll(element, options);
+
+$('#element').foundation('scrollToLoc', loc, options, callback);
